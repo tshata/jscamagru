d0f69e95b0102cc4dce05522394a143a8c929a1a
Tsepo SHATA <tshata@c4r7s11.wethinkcode.co.za>
2019-05-19 16:27:10 +0200

    continue with oauth

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var db = mongoose.connection;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var webcam = require('./public/js/webcam');
 
 //Init App
 var app = express();
